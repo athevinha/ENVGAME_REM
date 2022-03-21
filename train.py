ad7ecf9ce59c1ffeb20b2159403274e557df26d7
athevinha <nguyenthevinh1002@gmail.com>
2022-03-21 23:46:23 +0700

    add aug

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from keras.layers.core import Dense
 from keras.layers.core import Dropout
 from keras.layers.core import Flatten
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import SGD
 from keras.models import Sequential
 from keras.layers import BatchNormalization
 from keras.layers.convolutional import Conv2D
@@ -47,32 +48,27 @@ class create_model:
                       validation_split=0.2,
                       image_size=(img_height, img_width),
                       batch_size=batch_size)
+
     self.num_classes = len(self.train_ds.class_names)
-    self.train_ds = self.train_ds.map(lambda x, y: (x, tf.one_hot(y, depth=self.num_classes)))
-    self.val_ds = self.val_ds.map(lambda x, y: (x, tf.one_hot(y, depth=self.num_classes)))
-
-    self.val_batches = tf.data.experimental.cardinality(self.val_ds)
-    print(self.num_classes)
-    normalization_layer = tf.keras.layers.Rescaling(1./255)
-    self.train_ds = self.train_ds.map(lambda x, y: (normalization_layer(x), y)) # Where x—images, y—labels.
-    self.val_ds = self.val_ds.map(lambda x, y: (normalization_layer(x), y)) # Where x—images, y—labels.
-    AUTOTUNE = tf.data.AUTOTUNE
-
-
-    self.train_ds = self.train_ds.prefetch(buffer_size=AUTOTUNE)
-    self.val_ds = self.val_ds.prefetch(buffer_size=AUTOTUNE)
-
 
     self.data_augmentation = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1./255),
         tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-        tf.keras.layers.CenterCrop(self.img_height - 10, self.img_width-10 ),
+        tf.keras.layers.CenterCrop(self.img_height, self.img_width ),
         tf.keras.layers.RandomZoom(.5, .2)
     ])
+    self.train_ds = self.train_ds.map(lambda x, y: (self.data_augmentation(x), tf.one_hot(y, depth=self.num_classes)))
+    self.val_ds = self.val_ds.map(lambda x, y: (self.data_augmentation(x), tf.one_hot(y, depth=self.num_classes)))
+    self.val_batches = tf.data.experimental.cardinality(self.val_ds)
+
+    # print(self.num_classes)
+    AUTOTUNE = tf.data.AUTOTUNE
+    self.train_ds = self.train_ds.prefetch(buffer_size=AUTOTUNE)
+    self.val_ds = self.val_ds.prefetch(buffer_size=AUTOTUNE)
+    # print(train_ds)
 
     # self.train_ds = self.prepare(self.train_ds, shuffle=True, augment=True)
     
-  #Mobilenet
 
   def mobileNet(self,type_model):
     base_model = tf.keras.applications.mobilenet.MobileNet(input_shape=(self.img_height, self.img_width, 3),include_top=False,weights='imagenet')
@@ -86,7 +82,7 @@ class create_model:
     for layer in base_model.layers:
       layer.trainable = False
       
-    opt = RMSprop(0.001)
+    opt = tf.keras.optimizers.Adam()
     model.compile(opt, 'categorical_crossentropy', ['accuracy'])
     H = model.fit(self.train_ds,
                 steps_per_epoch=len(self.train_ds),
@@ -100,7 +96,7 @@ class create_model:
       for layer in base_model.layers[-23:]:
           layer.trainable = True
 
-      model.compile(opt, 'categorical_crossentropy', ['accuracy'])
+      model.compile(SGD(0.001), 'categorical_crossentropy', ['accuracy'])
 
       H = model.fit(self.train_ds,
                   steps_per_epoch=len(self.train_ds),
@@ -137,7 +133,7 @@ class create_model:
     model  = Model(inputs=base_model.input, outputs=fcHead)
     # print(self.num_classes)
   
-    model.compile(optimizer=RMSprop(0.001),
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
@@ -153,7 +149,7 @@ class create_model:
       for layer in base_model.layers[-26:]:
           layer.trainable = True
 
-      model.compile(RMSprop(0.001), 'categorical_crossentropy', ['accuracy'])
+      model.compile(SGD(0.001), 'categorical_crossentropy', ['accuracy'])
 
       H = model.fit(self.train_ds,
                   steps_per_epoch=len(self.train_ds),
@@ -174,6 +170,58 @@ class create_model:
     with open("historys/" + self.name_model + "_resnet50.txt", 'w') as f:
       f.write(str(traning_result))
     return traning_result
+
+  def envgame_leaf_disease(self):
+    # # model.add(Dense(1024,activation=('relu'),input_dim=512))
+    # model.add(Dense(self.num_classes,activation=('softmax')))
+    base_model = tf.keras.models.load_model('envgame.h5')
+    # input_img = tf.keras.Input(batch_shape=(None,self.img_height,self.img_width,3))
+    # ouput_img = base_model(input_img)
+    # base_model = Model(input_img, ouput_img)
+
+    fcHead = base_model.layers[-2].output
+    fcHead = Flatten(name='flatten')(fcHead)
+    # fcHead = Dense(256, activation='relu')(fcHead)
+    fcHead = Dense(self.num_classes, activation='softmax')(fcHead)
+    model = Model(inputs=base_model.input, outputs=fcHead)
+    # base_fake_model = Model(base_model.input, output_final)
+    print(self.img_height)
+    
+    for layer in model.layers: # -23
+        layer.trainable = True
+       
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    H = model.fit(self.train_ds,
+                steps_per_epoch=len(self.train_ds),
+                validation_data=self.val_ds,
+                validation_steps=self.val_batches,
+                epochs=self.epoch,
+    )
+    # print(H.history['val_accuracy'][-1])
+    if(H.history['accuracy'][-1] <= 0.85):
+      # print(en(base_model.layers))
+      for layer in base_model.layers[-23:]:
+          layer.trainable = True
+      model.compile(SGD(0.001), 'categorical_crossentropy', ['accuracy'])
+
+      H = model.fit(self.train_ds,
+                  steps_per_epoch=len(self.train_ds),
+                  validation_data=self.val_ds,
+                  validation_steps=self.val_batches,
+                  epochs=self.epoch,
+      )
+    path_model = "models/"+ self.name_model + "_envgame_leaf_disease.h5"
+    model.save(path_model)
+    traning_result = {
+      'log': H.history,
+      'model': path_model,
+      'name_model': self.name_model + "_envgame_leaf_disease.h5",
+      'type':'envgame_leaf_disease',
+    }
+    with open("historys/" + self.name_model + "_envgame_leaf_disease.txt", 'w') as f:
+      f.write(str(traning_result))
+    return traning_result
+
 
   # InceptionV3
 
@@ -206,6 +254,8 @@ class create_model:
     path_model = "models/"+ self.name_model + "_inceptionV3.h5"
     model.save(path_model)
     print(H.history)
+    # for layer in model.layers[:-23]: # -23
+  #       layer.trainable = False
     traning_result = {
       "log": H.history,
       "model": path_model,
@@ -215,62 +265,6 @@ class create_model:
     with open("historys/" + self.name_model + "_inceptionV3.txt", 'w') as f:
       f.write(str(traning_result))
     return traning_result
-
-
-
-  def envgame_leaf_disease(self):
-    # # model.add(Dense(1024,activation=('relu'),input_dim=512))
-    # model.add(Dense(self.num_classes,activation=('softmax')))
-    base_model = tf.keras.models.load_model('envgame.h5')
-    # input_img = tf.keras.Input(batch_shape=(None,self.img_height,self.img_width,3))
-    # ouput_img = base_model(input_img)
-    # base_model = Model(input_img, ouput_img)
-
-    fcHead = base_model.layers[-2].output
-    fcHead = Flatten(name='flatten')(fcHead)
-    # fcHead = Dense(256, activation='relu')(fcHead)
-    fcHead = Dense(self.num_classes, activation='softmax')(fcHead)
-    model = Model(inputs=base_model.input, outputs=fcHead)
-    # base_fake_model = Model(base_model.input, output_final)
-    print(self.img_height)
-    
-    for layer in model.layers: # -23
-        layer.trainable = True
-       
-    model.compile(optimizer=RMSprop(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-    H = model.fit(self.train_ds,
-                steps_per_epoch=len(self.train_ds),
-                validation_data=self.val_ds,
-                validation_steps=self.val_batches,
-                epochs=self.epoch,
-    )
-    # print(H.history['val_accuracy'][-1])
-    if(H.history['accuracy'][-1] <= 0.85):
-      # print(en(base_model.layers))
-      for layer in base_model.layers[-23:]:
-          layer.trainable = True
-      model.compile(tf.keras.optimizers.Adam(), 'categorical_crossentropy', ['accuracy'])
-
-      H = model.fit(self.train_ds,
-                  steps_per_epoch=len(self.train_ds),
-                  validation_data=self.val_ds,
-                  validation_steps=self.val_batches,
-                  epochs=self.epoch,
-      )
-    path_model = "models/"+ self.name_model + "_envgame_leaf_disease.h5"
-    model.save(path_model)
-    traning_result = {
-      'log': H.history,
-      'model': path_model,
-      'name_model': self.name_model + "_envgame_leaf_disease.h5",
-      'type':'envgame_leaf_disease',
-    }
-    with open("historys/" + self.name_model + "_envgame_leaf_disease.txt", 'w') as f:
-      f.write(str(traning_result))
-    return traning_result
-
-
-
 
   # def mobileNet(self,type_model):
   #   base_model = tf.keras.applications.mobilenet.MobileNet(input_shape=(self.img_height, self.img_width, 3))
